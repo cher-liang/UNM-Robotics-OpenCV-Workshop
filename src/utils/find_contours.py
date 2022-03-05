d8cab424ab326df5e4554ade6ea57d9ef42fe65b
CherLiang <liang14032@gmail.com>
2022-03-05 21:13:24 +0800

    red box still partly visible

--- src/utils/find_contours.py
+++ src/utils/find_contours.py
@@ -1,13 +1,12 @@
 import cv2 as cv
 
 def find_cnt(img,mask,show_bool=True):
-    ROI=img.copy()
     contours,_=cv.findContours(mask,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
     cnt=max(contours,key=cv.contourArea)
-    cv.drawContours(ROI,[cnt],-1,(0,0,255),5)
+    cv.drawContours(img,[cnt],-1,(0,0,255),5)
 
     if show_bool:
-        cv.imshow("ROI",ROI)
+        cv.imshow("ROI",img)
         cv.waitKey(0)
         cv.destroyAllWindows()
 
