eecb024eff010fa3d3b3d76dc65377dd6f3aed91
CherLiang <liang14032@gmail.com>
2022-03-05 16:38:14 +0800

    changes to overall code

--- src/workshop.py
+++ src/workshop.py
@@ -1,6 +1,11 @@
-from utils.show_bgr_hsv import show_bgr_hsv
 from utils.color_trackbar import color_trackbar
-from demo.face_detection import face_detection
+from utils.thresh_trackbar import thresh_trackbar
+
+from utils.demo.show_bgr_hsv import show_bgr_hsv
+from utils.demo.face_detection import face_detection
+from utils.demo.feature_matching import feature_matching
+
+from ocr import ocr
 
 import os
 import argparse
@@ -11,21 +16,25 @@ def main(args):
 
     img_path=os.path.join("data/test images/", args.image)
     
-    if args.module[0]=="all":
-        pass
+    if args.module[0]=="ocr":
+        ocr(img_path)
     elif args.module[0]=="hsv":
         show_bgr_hsv(img_path)
-    elif args.module[0]=="trackbar":
+    elif args.module[0]=="trackbar_color":
         color_trackbar(img_path)
+    elif args.module[0]=="trackbar_threshold":
+        thresh_trackbar(img_path)
     elif args.module[0]=="face_detection":
         face_detection()
+    elif args.module[0]=="feature_matching":
+        feature_matching()
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='UNM Robotics OpenCV Workshop')
-    parser.add_argument("-m","--module",dest="module",nargs=1,choices=["hsv","trackbar","face_detection","all"],default=["all"])
+    parser.add_argument("-m","--module",dest="module",nargs=1,choices=["hsv","trackbar_color","trackbar_threshold","face_detection","feature_matching","ocr"],default=["ocr"])
     parser.add_argument(
         '--image', help='Path to the input image.', default="book.jpg")
     args = parser.parse_args()
