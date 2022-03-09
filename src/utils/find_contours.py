29372aabdb5365790ff021a52b1eab2b80c6a16a
CherLiang <liang14032@gmail.com>
2022-03-09 23:31:43 +0800

    contour comment

--- src/utils/find_contours.py
+++ src/utils/find_contours.py
@@ -2,7 +2,7 @@ import cv2 as cv
 
 def find_cnt(img,mask,show_bool=True):
     contours,_=cv.findContours(mask,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-    cnt=max(contours,key=cv.contourArea)
+    cnt=max(contours,key=cv.contourArea) # find the contour with the largest area 
     cv.drawContours(img,[cnt],-1,(0,0,255),5)
 
     if show_bool:
